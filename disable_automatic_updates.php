e3fa7220e0cbc378a4f30501d73cc1e9ba605890
Ashley <Ashley@Ashleys-MacBook-Air.local>
2016-01-28 13:04:38 -0600

    disable WP updates function

---
+++ disable_automatic_updates.php
@@ -0,0 +1,6 @@
+<?php
+//Disable automatic WordPress plugin updates
+add_filter( 'auto_update_plugin', '__return_false' );
+
+//Disable automatic WordPress theme updates
+add_filter( 'auto_update_theme', '__return_false' );
