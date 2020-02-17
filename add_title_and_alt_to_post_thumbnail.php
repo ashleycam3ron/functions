3972b8301abd563149413803873972ecbd0e1d29
Ashley N Cameron <acameron@live.com>
2020-02-17 16:10:10 -0600

    Update add_title_and_alt_to_post_thumbnail.php

--- add_title_and_alt_to_post_thumbnail.php
+++ add_title_and_alt_to_post_thumbnail.php
@@ -1,8 +1,11 @@
-<?php function ac_add_img_title( $attr, $attachment = null ) {
+<?php /* Add Title and Alt Attribute to WordPress Image the_post_thumbnail */
+function z3_add_img_title( $attr, $attachment = null ) {
+ 
     $img_title = trim( strip_tags( $attachment->post_title ) );
-    $attr['title'] = $img_title;
     // or get the title instead of image title $attr['title'] = the_title_attribute( 'echo=0' );
+    $attr['title'] = $img_title;
     $attr['alt'] = $img_title;
+ 
     return $attr;
 }
-add_filter( 'wp_get_attachment_image_attributes','ac_add_img_title', 10, 2 );
+add_filter( 'wp_get_attachment_image_attributes','z3_add_img_title', 10, 2 );
